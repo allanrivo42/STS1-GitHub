024ff4945956065fb9ddf4487462f8629037045d
thomasss3s <80945798+thomasss3s@users.noreply.github.com>
2021-03-26 08:36:13 +0100

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -20,4 +20,11 @@
 
 
 
-//Fonctions réalisées par l'étudiant 3 :
+//Fonctions réalisées par l'étudiant 3 (Thomas):
+bool estImpair(int nb);
+
+float moyenne(int nb1, int nb2, int nb3);
+
+float reduction(int prix, int reduc);
+
+int nombreMedian(int a, int b, int c);
