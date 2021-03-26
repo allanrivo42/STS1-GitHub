ded899d8d5599ecdeeff5b90955cc2f717c10d75
thomasss3s <80945798+thomasss3s@users.noreply.github.com>
2021-03-26 09:36:33 +0100

    J'ai finis les fonctions de l'étudiant 3

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -21,10 +21,75 @@
 
 
 //Fonctions réalisées par l'étudiant 3 (Thomas):
-bool estImpair(int nb);
+bool estImpair(int nb)
+{
+int reste = nb % 2;
+            if (reste == 0) {
+                return true;
+            } else {
+                return false;
+            }
+}
 
-float moyenne(int nb1, int nb2, int nb3);
+			
+float moyenne(int nb1, int nb2, int nb3)
+{
+  int moyenne = (nb1 + nb2 + nb3)/3;
+    return moyenne;
+}
 
-float reduction(int prix, int reduc);
 
-int nombreMedian(int a, int b, int c);
+float reduction(int prix, int reduc)
+{
+  int pris = prix - prix*reduc/100;
+    return pris;
+}
+
+int nombreMedian(int a, int b, int c)
+{
+  if (a<b) 
+  {
+    if (a>c) 
+    {
+      return a;
+    }
+	else 
+	{
+	if (b<c) 
+      {
+      return b;
+    }
+      else 
+      {
+        return c;
+      }
+	}
+  }
+  else //a>b
+  {
+      if (a<c)
+      {
+          return a;
+      }
+      else //a>b a>c
+      {
+          if (b<c) //a>b a>c b<c a>c>b
+      {
+      return c;
+    }
+      else //a>b a>c b>c a>b>c
+      {
+        return b;
+      }
+      }
+  }
+  
+}
+      else 
+      {
+        return c;
+           }
+    }
+  }
+}
+          
